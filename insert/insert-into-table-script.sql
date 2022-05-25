34d381d862aec7395aebf62125269285c0a4703e
Hikmet <hikmet_cakir@outlook.com>
2022-05-25 15:10:10 +0300

    Added Insert Operation

---
+++ insert/insert-into-table-script.sql
@@ -0,0 +1,11 @@
+-- Example 1
+INSERT INTO cities (name, country, population, area)
+VALUES ('Munich', 'Germany', 13201000, 0013);
+
+
+-- Example 2
+INSERT INTO cities (name, country, population, area)
+VALUES
+  ('Munich', 'Germany', 13201000, 0013),
+  ('Shanghai', 'China', 22125000, 4015),
+  ('Paris', 'France', 10230000, 1023);
