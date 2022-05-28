1834ca5e1851b49e838677731715ef3c12f514a5
Hikmet <hikmet_cakir@outlook.com>
2022-05-28 19:53:35 +0300

    Added Join Types (INNER, LEFT, RIGHT, FULL)

--- select/Join And Aggregation/join-and-aggregation-sql-script.sql
+++ select/Join And Aggregation/join-and-aggregation-sql-script.sql
@@ -9,3 +9,57 @@ JOIN users ON users.id = comments.user_id;
 SELECT contents, url 
 FROM comments 
 JOIN photos ON photos.id = comments.photo_id;
+
+
+-- Example 3
+SELECT id
+FROM photos
+JOIN comments ON photos.id = comments.photo_id;
+
+-- ERROR!! column reference "id" is ambiguous
+
+
+-- Example 4
+SELECT photos.id
+FROM photos
+JOIN comments ON photos.id = comments.photo_id;
+
+-- SUCCESS 
+
+
+-- Example 5
+SELECT comments.id AS comment_id, photos.id
+FROM photos
+JOIN comments ON photos.id = comments.photo_id;
+
+-- SUCCESS 
+
+
+-- Example 6
+SELECT comments.id AS comment_id, p.id
+FROM photos AS p
+JOIN comments ON p.id = comments.photo_id;
+
+-- SUCCESS 
+
+-- Example 7
+SELECT comments.id AS comment_id, p.id
+FROM photos p
+JOIN comments ON p.id = comments.photo_id;
+
+-- SUCCESS 
+
+
+-- Example 8
+SELECT url, username
+FROM photos
+JOIN users ON users.id = photos.user_id;
+
+
+-- Example 9
+INSERT INTO photos (url, user_id)
+VALUES ('https://banner.jpgh', NULL);
+ 
+SELECT url, username
+FROM photos
+JOIN users ON users.id = photos.user_id;
