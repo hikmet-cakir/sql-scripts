da85352a99c6cd5edc3fdc998a05cb380f65fdad
Hikmet Çakır <hikmet_cakir@outlook.com>
2022-07-12 14:54:00 +0300

    SQL insert table structure was added (#6)

---
+++ insert-table/scripts.sql
@@ -0,0 +1,102 @@
+====================================================
+
+Example 1
+
+====================================================
+INSERT INTO cities (name, country, population, area)
+VALUES ('Munich', 'Germany', 13201000, 0013);
+====================================================
+
+Example 2
+
+====================================================
+INSERT INTO cities (name, country, population, area)
+VALUES
+  ('Munich', 'Germany', 13201000, 0013),
+  ('Shanghai', 'China', 22125000, 4015),
+  ('Paris', 'France', 10230000, 1023);
+====================================================
+
+Example 3
+
+====================================================
+INSERT INTO users (username)
+VALUES 
+  ('monahan93'),
+  ('pferrer'),
+  ('si93onis'),
+  ('99stroman');
+====================================================
+
+Example 4
+
+====================================================
+INSERT INTO videos (url, user_id)
+VALUES
+    ('http://one.jpg', 4);
+====================================================
+
+Example 5
+
+====================================================
+INSERT INTO videos (url, user_id)
+VALUES 
+  ('http://two.jpg', 1),
+  ('http://25.jpg', 1),
+  ('http://36.jpg', 1),
+  ('http://234.jpg', 2),
+  ('http://123.jpg', 3),
+  ('http://534.jpg', 4);
+====================================================
+
+Example 6
+
+====================================================
+INSERT INTO videos (url, user_id)
+VALUES 
+  ('http://two.jpg', 1),
+  ('http://25.jpg', 1),
+  ('http://36.jpg', 1),
+  ('http://234.jpg', 2),
+  ('http://123.jpg', 3),
+  ('http://534.jpg', 4);
+====================================================
+
+Example 7
+
+====================================================
+INSERT INTO users (username) 
+VALUES 
+        ('Reyna.Marvin'),
+        ('Micah.Cremin'),
+        ('Alfredo66'),
+        ('Gerard_Mitchell42'),
+        ('Frederique_Donnelly');
+====================================================
+
+Example 8
+
+====================================================
+INSERT INTO videos (url, user_id)
+VALUES
+        ('https://santina.net', 3),
+        ('https://alayna.net', 5),
+        ('https://kailyn.name', 3),
+        ('http://marjolaine.name', 1),
+        ('http://chet.net', 5),
+        ('http://jerrold.org', 2),
+        ('https://meredith.net', 4),
+        ('http://isaias.net', 4),
+        ('http://dayne.com', 4),
+        ('http://colten.net', 2),
+        ('https://adelbert.biz', 5),
+        ('http://kolby.org', 1),
+        ('https://deon.biz', 2),
+        ('https://marina.com', 5),
+        ('http://johnson.info', 1),
+        ('https://linda.info', 2),
+        ('https://tyrique.info', 4),
+        ('http://buddy.info', 5),
+        ('https://elinore.name', 2),
+        ('http://sasha.com', 3);
+====================================================
